34f14b1b9b821362b00925a3fa37f0abec73b15f
Senny Kalidien <senny@directzichtbaar.nl>
2016-05-31 00:22:53 +0200

    GET request for API

--- routes/api.js
+++ routes/api.js
@@ -2,14 +2,16 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-router.get('/feed', function(req, res, next) {
+router.get('/scores', function(req, res, next) {
     fs.readFile('https://api.leaguevine.com/v1/games/?tournament_id=20059&access_token=bcfb4d45e5', 'utf8', function(err, data) {
         if(err) {
             res.status(404);
             next();
         }
 
-        res.render('feed', { title: 'Feed', items: JSON.parse(data), layout: false });
+        console.log(data);
+
+        res.render('matches', { title: 'Feed', items: JSON.parse(data), layout: false });
     });
 });
 
