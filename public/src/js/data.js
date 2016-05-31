b21a9bad065533eb62e0e8076f4cd3e8492490e9
Fons Hettema <fons.hettema@hva.nl>
2016-05-31 13:27:00 +0200

    Beautify

--- public/src/js/data.js
+++ public/src/js/data.js
@@ -1,33 +1,33 @@
 /*********************************************************
 	DATA REQUEST
 *********************************************************/
-UFA.data = ( () => {
+UFA.data = (() => {
 
     function socket() {
-		// Connect with socket
-		var socket = io.connect("http://localhost:3010"),
-		    submit = document.querySelector("#submit");
+        // Connect with socket
+        var socket = io.connect("http://localhost:3010"),
+            submit = document.querySelector("#submit");
 
-		submit.addEventListener("click", function(e) {
-		    e.preventDefault();
-		    var score1 = document.querySelector("#team1").value,
-		        score2 = document.querySelector("#team2").value
-		    addScore(score1, score2);
-		});
+        submit.addEventListener("click", function(e) {
+            e.preventDefault();
+            var score1 = document.querySelector("#team1").value,
+                score2 = document.querySelector("#team2").value
+            addScore(score1, score2);
+        });
 
-		// Add score (min or plus for teams)
-		function addScore(score1, score2) {
-		    // Send score to socket
-		    socket.emit('addScore', {
-		        score1: score1,
-		        score2: score2,
-		        time: Date.now()
-		    });
-		}
+        // Add score (min or plus for teams)
+        function addScore(score1, score2) {
+            // Send score to socket
+            socket.emit('addScore', {
+                score1: score1,
+                score2: score2,
+                time: Date.now()
+            });
+        }
 
-		socket.on('broad', function(data) {
-		  // Do stuff with data on the client
-		});
+        socket.on('broad', function(data) {
+            // Do stuff with data on the client
+        });
     }
 
     return {
