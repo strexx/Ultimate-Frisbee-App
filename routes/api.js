ae9359340ea248d7f0bf58d5cb8129b94c67689b
melvinr <melvinr@096.124.028.145.hva.nl>
2016-05-31 13:01:07 +0200

    Changed API link, fixed rendering
    found a way to get the right matches when needed

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var fs = require('fs');
 
 router.get('/scores', function(req, res, next) {
-    fs.readFile('https://api.leaguevine.com/v1/games/?tournament_id=20059&access_token=bcfb4d45e5', 'utf8', function(err, data) {
+    fs.readFile('https://api.leaguevine.com/v1/games/?tournament_id=20059&starts_after=2016-06-04T15%3A00%3A00%2B02%3A00&order_by=%5Bstart_time%5D&limit=20&access_token=6dc9d3795a', 'utf8', function(err, data) {
         if(err) {
             res.status(404);
             next();
