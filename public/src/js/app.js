092af6402526551b1fa7d0c3cc31e991c1513061
melvinr <melreijnoudt@gmail.com>
2016-06-02 21:36:19 +0200

    Create real-time score update on the client side

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -13,6 +13,7 @@ UFA.launcher = ( () => {
     function init() {
         document.addEventListener("DOMContentLoaded", function () {
             UFA.ux.init();
+            UFA.data.socket();
             UFA.ux.toggleMenu();
             UFA.router.init();
         });
