5644fba006a55a5c4ee974fa46d5782de4ff10d1
Fons Hettema <fons.hettema@hva.nl>
2016-06-08 13:08:24 +0200

    Login feedback v0.9

--- public/src/js/login.js
+++ public/src/js/login.js
@@ -4,13 +4,15 @@
 UFA.login = (() => {
     // Global vars
     var submit = document.querySelector('.submit'),
-        placeholder = document.querySelector('.submit i');
+        placeholder = document.querySelector('.submit i'),
+        feedback = document.querySelector('.feedback');
 
     submit.addEventListener("click", loginSubmit, false);
 
     function loginSubmit(e) {
       placeholder.classList.remove("fa-long-arrow-right");
       placeholder.classList.add("fa-check");
+      feedback.classList.add("is-visible");
     }
 
     return {
