25230dd5fea7517ee302d509a5e6d5118956179f
Senny Kalidien <senny@directzichtbaar.nl>
2016-06-10 00:54:44 +0200

    Removed unnecessary apiroutes

--- routes/api.js
+++ routes/api.js
@@ -10,9 +10,9 @@ var express = require('express'),
     url = 'mongodb://146.185.135.172:27017/ultifris';
 
 
-router.get('/matches/live', function(req, res) {
+router.get('/matches', function(req, res) {
   request({
-        url: 'https://api.leaguevine.com/v1/games/?tournament_id=19746&starts_after=2015-06-12T11%3A00%3A00%2B02%3A00&order_by=%5Bstart_time%5D&limit=5&access_token=6dc9d3795a',
+        url: 'https://api.leaguevine.com/v1/games/?tournament_id=19746&starts_before=2015-06-12T23%3A59%3A59-02%3A00&starts_after=2015-06-12T00%3A00%3A00-02%3A00&order_by=%5Bstart_time%5D&limit=80&access_token=6dc9d3795a',
         json: true
     }, function(error, response, data) {
         if (!error && response.statusCode == 200) {
@@ -31,47 +31,6 @@ router.get('/matches/live', function(req, res) {
     });
 });
 
-router.get('/matches/recent', function(req, res) {
-    request({
-        url: 'https://api.leaguevine.com/v1/games/?tournament_id=19746&starts_before=2015-06-12T11%3A00%3A00%2B02%3A00&order_by=%5B-start_time%5D&limit=5&access_token=6dc9d3795a',
-        json: true
-    }, function(error, response, data) {
-        if (!error && response.statusCode == 200) {
-            var objects = data.objects;
-
-            for (var key in objects) {
-                objects[key].start_time = dateFormat(objects[key].start_time, "HH:MM");
-                objects[key].game_site.name = objects[key].game_site.name.split('.')[0];
-            }
-            res.render('matches', {
-                title: 'Matches',
-                items: objects,
-                layout: false
-            });
-        }
-    });
-});
-
-router.get('/matches/upcoming', function(req, res) {
-    request({
-        url: 'https://api.leaguevine.com/v1/games/?tournament_id=19746&starts_after=2015-06-12T14%3A00%3A00%2B02%3A00&order_by=%5Bstart_time%5D&limit=5&access_token=6dc9d3795a',
-        json: true
-    }, function(error, response, data) {
-        if (!error && response.statusCode == 200) {
-            var objects = data.objects;
-
-            for (var key in objects) {
-                objects[key].start_time = dateFormat(objects[key].start_time, "HH:MM");
-                objects[key].game_site.name = objects[key].game_site.name.split('.')[0];
-            }
-            res.render('matches', {
-                title: 'Matches',
-                items: objects,
-                layout: false
-            });
-        }
-    });
-});
 
 router.get('/tournaments', function(req, res) {
     request({
