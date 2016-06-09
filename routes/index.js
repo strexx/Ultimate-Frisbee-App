340db9d732659033bc31be4cb8b5acf16a71058d
Senny Kalidien <senny@directzichtbaar.nl>
2016-06-09 14:44:21 +0200

    Created Tournament routes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var express = require('express'),
     request = require('request'),
     dateFormat = require('dateformat');
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     console.log(req.session.user_id);
     request({url: 'https://api.leaguevine.com/v1/games/?tournament_id=19746&starts_after=2015-06-12T11%3A00%3A00%2B02%3A00&order_by=%5Bstart_time%5D&limit=5&access_token=6dc9d3795a', json: true, timeout: 6000}, function (error, response, data) {
         if (!error && response.statusCode == 200) {
@@ -22,7 +22,7 @@ router.get('/', function (req, res, next) {
     });
 });
 
-router.get('/match/:gameID', function (req, res, next) {
+router.get('/match/:gameID', function (req, res) {
     var gameID = req.params.gameID,
         session = req.session.user_id;
 
@@ -37,7 +37,7 @@ router.get('/match/:gameID', function (req, res, next) {
     });
 });
 
-router.get('/tournaments', function (req, res, next) {
+router.get('/tournaments', function (req, res) {
     request({url: 'https://api.leaguevine.com/v1/tournaments/?tournament_ids=%5B19753%2C19751%2C19752%5D&access_token=bbe603bb50', json: true}, function (error, response, data) {
         if (!error && response.statusCode == 200) {
           var objects = data.objects;
@@ -47,7 +47,31 @@ router.get('/tournaments', function (req, res, next) {
     });
 });
 
-router.get('/login', function (req, res, next) {
+router.get('/tournament/:tournamentID', function (req, res) {
+    var tournamentID = req.params.tournamentID;
+    console.log(tournamentID);
+
+    request({url: 'https://api.leaguevine.com/v1/games/?tournament_id='+ tournamentID +'&order_by=%5Bstart_time%5D&limit=100&access_token=6dc9d3795a', json: true}, function (error, response, data) {
+        if (!error && response.statusCode == 200) {
+          var objects = data.objects;
+
+          console.log(objects);
+
+          for(var key in objects) {
+            objects[key].start_time = dateFormat(objects[key].start_time, "HH:MM");
+            if(objects[key].game_site !== null) {
+                objects[key].game_site.name = objects[key].game_site.name.split('.')[0];
+            }
+          }
+          res.render('tournament', { title: 'Tournament', items: objects });
+        } else {
+          res.render('error', { title: 'Error', error: error });
+          console.log(error);
+        }
+    });
+});
+
+router.get('/login', function (req, res) {
     res.render('login', { title: 'Login' });
 });
 
