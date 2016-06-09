6eb687c86a5bf9cb37bcccd38b4b2c1668c3ad35
Senny Kalidien <senny@directzichtbaar.nl>
2016-06-10 00:54:27 +0200

    Deleted console.logs

--- routes/index.js
+++ routes/index.js
@@ -11,86 +11,58 @@ var express = require('express'),
 router.get('/', function(req, res, next) {
     var matchArray = [];
 
-
-    // var bla = [{
-    //     "object1": [ // 10:30
-    //         {}, {}, {}
-    //     ],
-    //     "object2": [ // 12:30
-    //         {}, {}, {}
-    //     ],
-    //     "object3": [ // 14:30 & 18:30
-    //         {}, {}, {}
-    //     ]
-    // }];
-
-    //console.log(recentMatches);
-
-    // var liveMatches = matchArray.map(function(obj) {
-    //     var newObj = {};
-    //     newObj[obj.key] = obj.value;
-    //     return newObj;
-    // });
-    //
-    // var upcomingMatches = matchArray.map(function(obj) {
-    //     var newObj = {};
-    //     newObj[obj.key] = obj.value;
-    //     return newObj;
-    // });
-
     var findMatches = function(db, callback) {
         var collectionCursor = db.collection('matches').find();
         collectionCursor.each(function(err, match) {
             if (match != null) {
-              matchArray.push(match);
-              //console.log(match);
+                matchArray.push(match);
 
             } else {
-              callback();
+                callback();
             }
         });
     };
 
     MongoClient.connect(url, function(err, db) {
-            if (err) {
-                console.log('Unable to connect to the mongoDB server. Error:', err);
-            } else {
-                console.log('Connection established!');
+        if (err) {
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+            console.log('Connection established!');
 
-                findMatches(db, function() {
-                    // console.log(match);
-                    var recentTime = "10:00";
-                    var liveTime = "12:30";
-                    //var upcomingTime = "14:30";
+            findMatches(db, function() {
+                // console.log(match);
+                var recentTime = "10:00";
+                var liveTime = "12:30";
+                //var upcomingTime = "14:30";
 
-                    var recentMatches = matchArray.filter(function(obj) {
-                        return obj.start_time < liveTime;
-                    });
-                    var liveMatches = matchArray.filter(function(obj) {
-                        return obj.start_time == liveTime;
-                    });
-                    var upcomingMatches = matchArray.filter(function(obj) {
-                        return obj.start_time > liveTime;
-                    });
+                var recentMatches = matchArray.filter(function(obj) {
+                    return obj.start_time < liveTime;
+                });
+                var liveMatches = matchArray.filter(function(obj) {
+                    return obj.start_time == liveTime;
+                });
+                var upcomingMatches = matchArray.filter(function(obj) {
+                    return obj.start_time > liveTime;
+                });
 
-                    //console.log(recentMatches);
+                //console.log(recentMatches);
 
-                    res.render('matches', {
-                      title: 'Matches',
-                      items: liveMatches,
-                      recentMatches: recentMatches,
-                      liveMatches: liveMatches,
-                      upcomingMatches: upcomingMatches
+                res.render('matches', {
+                    title: 'Matches',
+                    items: liveMatches,
+                    recentMatches: recentMatches,
+                    liveMatches: liveMatches,
+                    upcomingMatches: upcomingMatches
 
-                    });
-                    db.close();
-                })
-            }
-          });
+                });
+                db.close();
+            });
+        }
+    });
 
 });
 
-router.get('/match/:gameID', function (req, res) {
+router.get('/match/:gameID', function(req, res) {
     var gameID = req.params.gameID,
         session = req.session.user_id;
 
@@ -115,9 +87,11 @@ router.get('/match/:gameID', function (req, res) {
     });
 });
 
-
-router.get('/tournaments', function (req, res) {
-    request({url: 'https://api.leaguevine.com/v1/tournaments/?tournament_ids=%5B19753%2C19751%2C19752%5D&access_token=bbe603bb50', json: true}, function (error, response, data) {
+router.get('/tournaments', function(req, res) {
+    request({
+        url: 'https://api.leaguevine.com/v1/tournaments/?tournament_ids=%5B19753%2C19751%2C19752%5D&access_token=bbe603bb50',
+        json: true
+    }, function(error, response, data) {
         if (!error && response.statusCode == 200) {
             var objects = data.objects;
             console.log(objects);
@@ -129,33 +103,41 @@ router.get('/tournaments', function (req, res) {
     });
 });
 
-
-router.get('/tournament/:tournamentID', function (req, res) {
+router.get('/tournament/:tournamentID', function(req, res) {
     var tournamentID = req.params.tournamentID;
     console.log(tournamentID);
 
-    request({url: 'https://api.leaguevine.com/v1/games/?tournament_id='+ tournamentID +'&order_by=%5Bstart_time%5D&limit=100&access_token=6dc9d3795a', json: true}, function (error, response, data) {
+    request({
+        url: 'https://api.leaguevine.com/v1/games/?tournament_id=' + tournamentID + '&order_by=%5Bstart_time%5D&limit=100&access_token=6dc9d3795a',
+        json: true
+    }, function(error, response, data) {
         if (!error && response.statusCode == 200) {
-          var objects = data.objects;
+            var objects = data.objects;
 
-          console.log(objects);
-
-          for(var key in objects) {
-            objects[key].start_time = dateFormat(objects[key].start_time, "HH:MM");
-            if(objects[key].game_site !== null) {
-                objects[key].game_site.name = objects[key].game_site.name.split('.')[0];
+            for (var key in objects) {
+                objects[key].start_time = dateFormat(objects[key].start_time, "HH:MM");
+                if (objects[key].game_site !== null) {
+                    objects[key].game_site.name = objects[key].game_site.name.split('.')[0];
+                }
             }
-          }
-          res.render('tournament', { title: 'Tournament', items: objects });
+            res.render('tournament', {
+                title: 'Tournament',
+                items: objects
+            });
         } else {
-          res.render('error', { title: 'Error', error: error });
-          console.log(error);
+            res.render('error', {
+                title: 'Error',
+                error: error
+            });
+            console.log(error);
         }
     });
 });
 
-router.get('/login', function (req, res) {
-    res.render('login', { title: 'Login' });
+router.get('/login', function(req, res) {
+    res.render('login', {
+        title: 'Login'
+    });
 });
 
 router.get('/logout', function(req, res) {
