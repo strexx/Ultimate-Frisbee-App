9e07db2c27e92f6d13b119e6b40d2bf6aa3c49cc
Senny Kalidien <senny@directzichtbaar.nl>
2016-06-09 20:54:11 +0200

    Added JS filter for each match
    Created three arrays for recent, live and upcoming matches and added
    the arrays to the render.

--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,52 @@ var express = require('express'),
     request = require('request'),
     dateFormat = require('dateformat'),
     mongodb = require('mongodb'),
-    //ObjectId = require('mongodb').ObjectID,
     MongoClient = mongodb.MongoClient,
-    //assert = require('assert'),
     url = 'mongodb://146.185.135.172:27017/ultifris';
-
 
 
 router.get('/', function(req, res, next) {
     var matchArray = [];
+
+
+    // var bla = [{
+    //     "object1": [ // 10:30
+    //         {}, {}, {}
+    //     ],
+    //     "object2": [ // 12:30
+    //         {}, {}, {}
+    //     ],
+    //     "object3": [ // 14:30 & 18:30
+    //         {}, {}, {}
+    //     ]
+    // }];
+
+    //console.log(recentMatches);
+
+    // var liveMatches = matchArray.map(function(obj) {
+    //     var newObj = {};
+    //     newObj[obj.key] = obj.value;
+    //     return newObj;
+    // });
+    //
+    // var upcomingMatches = matchArray.map(function(obj) {
+    //     var newObj = {};
+    //     newObj[obj.key] = obj.value;
+    //     return newObj;
+    // });
 
     var findMatches = function(db, callback) {
         var collectionCursor = db.collection('matches').find();
         collectionCursor.each(function(err, match) {
             if (match != null) {
               matchArray.push(match);
-              console.log(match);
+              //console.log(match);
 
             } else {
               callback();
             }
         });
     };
-
-
 
     MongoClient.connect(url, function(err, db) {
             if (err) {
@@ -37,30 +59,35 @@ router.get('/', function(req, res, next) {
 
                 findMatches(db, function() {
                     // console.log(match);
+                    var recentTime = "10:00";
+                    var liveTime = "12:30";
+                    //var upcomingTime = "14:30";
+
+                    var recentMatches = matchArray.filter(function(obj) {
+                        return obj.start_time < liveTime;
+                    });
+                    var liveMatches = matchArray.filter(function(obj) {
+                        return obj.start_time == liveTime;
+                    });
+                    var upcomingMatches = matchArray.filter(function(obj) {
+                        return obj.start_time > liveTime;
+                    });
+
+                    //console.log(recentMatches);
+
                     res.render('matches', {
                       title: 'Matches',
-                      items: matchArray
+                      items: liveMatches,
+                      recentMatches: recentMatches,
+                      liveMatches: liveMatches,
+                      upcomingMatches: upcomingMatches
+
                     });
-                    // console.log('matches were found');
                     db.close();
                 })
             }
           });
 
-        request({url: 'https://api.leaguevine.com/v1/games/?tournament_id=19746&starts_after=2015-06-12T11%3A00%3A00%2B02%3A00&order_by=%5Bstart_time%5D&limit=6&access_token=6dc9d3795a', json: true, timeout: 6000}, function (error, response, data) {
-              if (!error && response.statusCode == 200) {
-                var objects = data.objects;
-
-                for(var key in objects) {
-                  objects[key].start_time = dateFormat(objects[key].start_time, "HH:MM");
-                  objects[key].game_site.name = objects[key].game_site.name.split('.')[0];
-                }
-                res.render('matches', { title: 'Matches', items: objects });
-              } else {
-                res.render('error', { title: 'Error', error: error });
-                console.log(error);
-              }
-          });
 });
 
 router.get('/match/:gameID', function(req, res, next) {
