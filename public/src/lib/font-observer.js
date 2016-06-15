900a38662fcea537c39c3da1c18c925d7ee271c3
Fons Hettema <fons.hettema@hva.nl>
2016-06-15 13:04:56 +0200

    Fontfaceobserver setup

---
+++ public/src/lib/font-observer.js
@@ -0,0 +1,26 @@
+(function() {
+    var fontFamilies = {
+        'Lato': [{
+            weight: 400
+        }, {
+            weight: 300
+        }, {
+            weight: 700
+        }]
+    };
+
+    var fontObservers = [];
+
+    Object.keys(fontFamilies).forEach(function(family) {
+        fontObservers.push(fontFamilies[family].map(function(config) {
+            return new FontFaceObserver(family, config).check()
+        }));
+    });
+
+    Promise.all(fontObservers)
+        .then(function() {
+            document.documentElement.className += "fonts-loaded";
+        }, function() {
+            console.log('Fonts not available');
+        });
+}());
