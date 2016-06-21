a01ac829c565cc2982d60c28438c638cc2070839
melvinr <melreijnoudt@gmail.com>
2016-06-21 16:15:24 +0200

    Implemented Senny's fix for tournaments page

--- routes/index.js
+++ routes/index.js
@@ -170,15 +170,17 @@ router.get('/tournaments', function(req, res) {
 
         collectionCursor.each(function(err, match) {
             if (match !== null) {
-                var tournament = match.tournament;
-                var tournamentID = tournament.id;
-                var tournamentName = tournament.name;
 
-                tournamentIDs.push(tournamentID);
-                tournamentNames.push(tournamentName);
+              if (match.tournament !== null){
+                    var tournament = match.tournament;
+                    var tournamentID = tournament.id;
+                    var tournamentName = tournament.name;
+
+                    tournamentIDs.push(tournamentID);                     tournamentNames.push(tournamentName);
             } else {
                 callback();
             }
+          }
         });
     };
 
