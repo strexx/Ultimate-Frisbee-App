5ae8ab5c7a2b52c10f7b6863a1c4f8b9baacc4e0
melvinr <melreijnoudt@gmail.com>
2016-06-21 16:11:41 +0200

    Commented service worker

--- public/src/js/router.js
+++ public/src/js/router.js
@@ -6,7 +6,7 @@ UFA.router = (() => {
         var ID = window.location.pathname.split('/')[2];
 
         if (window.location.pathname == '/') {
-            UFA.sw.init();
+            // UFA.sw.init();
             UFA.page.matchesLive();
             console.log("Matches");
         } else if (window.location.pathname == '/login/') {
