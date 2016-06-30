7f858252dc75e0fe52ebfdb938c6c4eaafcbbe74
Senny Kalidien <senny@directzichtbaar.nl>
2016-07-01 00:45:05 +0200

    Fixed issue with toggle menu

--- public/src/js/pages.js
+++ public/src/js/pages.js
@@ -5,63 +5,52 @@ UFA.page = (() => {
 
     function matchesRecent() {
         UFA.ux.toggleClass();
-        UFA.ux.toggleMenu();
     }
 
     function matchesLive() {
         UFA.scores.changeHomeScores();
         UFA.ux.splashVisited();
         UFA.ux.toggleClass();
-        UFA.ux.toggleMenu();
     }
 
     function matchesUpcoming() {
         UFA.ux.toggleClass();
-        UFA.ux.toggleMenu();
     }
 
     function matchInfo() {
         UFA.ux.toggleClass();
-        UFA.ux.toggleMenu();
     }
 
     function matchScores() {
         UFA.scores.init();
         UFA.ux.toggleClass();
-        UFA.ux.toggleMenu();
     }
 
     function matchLocation() {
         UFA.ux.toggleClass();
-        UFA.ux.toggleMenu();
     }
 
     function tournaments() {
-
     }
 
     function tournamentMatches() {
         UFA.ux.toggleClass();
-        UFA.ux.toggleMenu();
     }
 
     function tournamentRounds() {
         UFA.ux.toggleClass();
-        UFA.ux.toggleMenu();
     }
 
     function tournamentRanking() {
         UFA.ux.toggleClass();
-        UFA.ux.toggleMenu();
     }
 
     function login() {
         UFA.ux.loginSubmit();
-        UFA.ux.toggleMenu();
     }
 
     function favorites () {
-      UFA.ux.toggleMenu();
+        
     }
 
     return {
