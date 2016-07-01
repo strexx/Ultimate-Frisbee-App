d0024d7ad65b6d31aaf64a6f29efb973faf2670f
Senny Kalidien <senny@directzichtbaar.nl>
2016-07-01 04:38:59 +0200

    Whyyyyyyy

--- public/sw.js
+++ public/sw.js
@@ -10,10 +10,16 @@ this.addEventListener('install', function (event) {
         '/dist/js/app.min.js',
         '/dist/lib/fontfaceobserver.min.js',
         '/dist/lib/socket.io.min.js',
+        '/dist/lib/modernizr.js',
+        '/dist/img/icons/matches.png',
+        '/dist/img/icons/tournaments.png',
+        '/dist/img/icons/login.png',
+        '/dist/img/icons/logout.png',
+        '/dist/img/icons/favorites.png',
         '/sw.js',
-        'https://fonts.googleapis.com/css?family=Lato:400,300,700',
+        'https://fonts.googleapis.com/css?family=Lato:100,300,400,700',
         'https://fonts.googleapis.com/css?family=Roboto+Slab:300',
-        'http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css'
+        'https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css'
       ]);
     })
   );
@@ -48,16 +54,29 @@ this.addEventListener('fetch', function(event) {
                     //console.log('found cached response', response);
                     return response;
                 } else {
-                    if (event.request.url.indexOf("socket.io") != -1) { // ignore socket polling
+                    if (event.request.url.indexOf("socket.io") !== -1) { // ignore socket polling
                         return fetch(event.request);
                     } else {
                         console.log('response not in cache, fetching it');
                         return fetchAndCache(event);
                     }
                 }
-            })
+            });
+        })
     );
 });
+
+// this.addEventListener('fetch', function(event) {
+//     event.respondWith(
+//         caches.match(event.request).then(function(res) {
+//             return res || fetch(event.request).then(function(response) {
+//                 if (event.request.url.indexOf("socket.io") == -1) { // ignore socket polling
+//                     return fetchAndCache(event);
+//                 }
+//             });
+//         })
+//     );
+// });
 
 function fetchAndCache(event) {
     return fetch(event.request).then(function(response) {
